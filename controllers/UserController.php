6026f71cf1b868ba42d556271101d29ec936a328
tiendeeptry119 <tienptph51041@gmail.com>
2024-11-12 21:47:24 +0700

    update code

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,13 +1,13 @@
 <?php
 include './models/UserModel.php';
-class YourController
+class UserController
 {
     public $UserModel;
     public function __construct()
     {
-        $this->UserModel = new YourModel();
+        $this->UserModel = new UserModel();
     }
     public function showAll(){
-        include "./views/home.php";
+        include "./views/client/home.php";
     }
 }
--- includes/connect_db.php
+++ includes/connect_db.php
@@ -10,7 +10,6 @@ function connectDB()
         $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
         return $conn;
     } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
