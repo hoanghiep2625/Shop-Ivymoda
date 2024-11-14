310d329b84da4a02743f1c142c6e689c401b938e
hoanghiep2625 <hoanghiep262005@gmail.com>
2024-11-15 01:51:01 +0700

    update

--- views/client/home.php
+++ views/client/home.php
@@ -1,2 +1,46 @@
-<?php echo $_SESSION['email']; ?>
-<h1>hello</h1>
+<?php require_once "./includes/header.php" ?>
+
+<body class="mx-[8%]">
+    <?php require_once "./includes/menu.php" ?>
+    <article>
+        <div class="grid grid-cols-[1fr_1.3fr_1fr] items-center justify-center">
+            <div class="bg-[#D73831] text-white p-2 font-semibold text-center">SALE OFF 50%</div>
+            <div class="bg-[#DC633A] text-white p-2 font-semibold text-center">SALE OFF 30%</div>
+            <div class="bg-[#AC2F33] text-white p-2 font-semibold text-center">LAST SALE FROM 100K</div>
+        </div>
+        <banner>
+            <img src="./public/image/banner1.webp" class="rounded-tl-[80px] rounded-br-[80px] my-7" alt="">
+        </banner>
+        <p class="text-center font-semibold text-3xl">NEW ARRIVAL</p>
+        <div class="flex justify-center pb-8">
+            <p class="pr-6 text-xl underline">IVY moda</p>
+            <p class="pl-6 text-xl text-gray-500">IVY men</p>
+        </div>
+        <div class="grid grid-cols-5 gap-4">
+            <div class="relative">
+                <img src="./public/image/sanpham1.webp" class="pb-2" alt="">
+                <div class="flex gap-2 py-2">
+                    <div class="rounded-full bg-red-500 w-5 h-5"></div>
+                    <div class="rounded-full bg-red-500 w-5 h-5"></div>
+                </div>
+                <div class="text-sm pb-4">Áo Sơ Mi Lụa Cổ Đổ Lucille</div>
+                <div class="font-semibold">9.999.999đ</div>
+                <div>
+                    <div class="w-8 h-8 bg-black hover:bg-white border border-transparent hover:border-black rounded-tl-[10px] rounded-br-[10px] absolute right-0 bottom-0 flex items-center justify-center transition-all duration-300 group">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="w-4 h-4 fill-current text-white group-hover:text-black transition-all duration-300">
+                            <path d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96zM252 160c0 11 9 20 20 20l44 0 0 44c0 11 9 20 20 20s20-9 20-20l0-44 44 0c11 0 20-9 20-20s-9-20-20-20l-44 0 0-44c0-11-9-20-20-20s-20 9-20 20l0 44-44 0c-11 0-20 9-20 20z" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </article>
+    <hr>
+</body>
+<?php require_once "./includes/footer.php" ?>
+
+</html>
