4a58bcc753970cb23a764ebccd9e6047dbe27895
thanhlam25 <thanhlam051204@gmail.com>
2024-11-16 16:04:46 +0700

    update-q&A-hdmh

--- views/client/q&a.php
+++ views/client/q&a.php
@@ -2,9 +2,179 @@
 
 <body class="mx-[8%]">
     <?php require_once "./includes/menu.php" ?>
-    <article>
-        hdshkfskfk
+    <hr>
+    <div class="flex gap-4 mb-[20px] mt-[20px] ">
+        <div class="text-sm">Trang chủ</div>
+        <div class="text-sm">-</div>
+        <div class="text-sm"> Q&A</div>
+    </div>
+    <hr>
+    <div class="mt-[10px] flex">
+        <img src="./public/image/Q&A.jpg" alt="">
+    </div>
+    <hr class="mb-[30px]">
+    <article class="flex grid grid-cols-[1fr_2fr] gap-10  justify-center">
+        <div class=" border border-gray-400 rounded-tl-[40px] rounded-br-[40px] h-80 ">
+            <p class="p-12">
+                <a class="leading-[15px] text-[14px] font-semibold text-gray-600 block hover:text-black" href="">Về IVY
+                    moda</a> <br>
+                <a class=" leading-[15px] text-[14px] font-semibold text-gray-600 block hover:text-black" href="">Chính
+                    sách thẻ thành viên</a> <br>
+                <a class=" leading-[15px] text-[14px] font-semibold text-gray-600 block hover:text-black" href="">Bảo
+                    hành trọn đời</a> <br>
+                <a class=" leading-[15px] text-[14px] font-semibold text-gray-600 block hover:text-black" href="">Chính
+                    sách đổi trả</a> <br>
+                <a class=" leading-[15px] text-[14px] font-semibold text-gray-600 block hover:text-black" href="">Hệ
+                    thống cửa hàng</a> <br>
+                <a class=" leading-[15px] text-[14px] font-semibold text-gray-600 block hover:text-black"
+                    href="">Q&A</a> <br>
+            </p>
+        </div>
+
+        <div class="mb-8">
+            <div class="text-2xl font-semibold">
+                Q&A
+            </div><br>
+            <div class="text-lg font-semibold">
+                I. TÀI KHOẢN CỦA TÔI
+            </div><br>
+            <div class="space-y-1.5">
+                <div class="text-[#6c757d] font-semibold text-base">
+                    1. Hướng dẫn cách thay đổi/ cập nhật số điện thoại liên kết với tài khoản IVY moda
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    2. Hướng dẫn cách thay đổi/cập nhật email liên kết với tài khoản IVY moda
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    3. Làm thế nào để tôi quản lý thông tin trong tài khoản?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    4. Làm cách nào để tôi thay đổi mật khẩu tại IVY moda?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    5. Tại sao tôi không đăng nhập được?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    6. Làm thế nào để đăng nhận xét sản phẩm trên <a class="text-black font-bold" href="">IVYmoda.com</a>?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    7. Làm cách nào để tôi hủy đơn hàng?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    8. Làm cách nào để tôi đăng ký tài khoản trên website <a class="text-black font-bold" href="">IVYmoda.com</a>?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    9. Làm cách nào để tôi có thể đăng nhập vào App IVY moda?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    10. Tại sao bài nhận xét sản phẩm của tôi không được hiển thị tại trang <a class="text-black font-bold" href="">IVYmoda.com</a>?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    11. Làm thế nào để tôi thay đổi địa chỉ nhận hàng mặc định?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    12. Cách cài đặt địa chỉ nhận hàng mặc định
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    13. Làm thế nào để có tài khoản thành viên của IVY moda?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    14. Làm thế nào để được tích điểm?
+                </div>
+            </div><br>
+
+            <div class="text-lg font-semibold">
+                II. ĐẶT HÀNG VÀ THANH TOÁN
+            </div><br>
+            <div class="space-y-1.5">
+                <div class="text-[#6c757d] font-semibold text-base">
+                    1. IVY moda hoàn tiền trong bao lâu?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    2. Làm thế nào để tôi đặt hàng qua website IVY moda?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    3. IVY moda cam kết bảo mật và an toàn như thế nào đối với việc thanh toán bằng thẻ?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    4. IVY moda hiện đang hỗ trợ các phương thức thanh toán nào?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    5. Tôi có thể thanh toán khi nhận hàng không?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    6. Làm thế nào để tôi thêm/ bớt sản phẩm?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    7. Làm thế nào để tôi thay đổi địa chỉ giao hàng?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    8. Làm thế nào để tôi nhận biết sản phẩm còn hay hết hàng?
+                </div>
+            </div><br>
+
+
+            <div class="text-lg font-semibold">
+                III. GIAO NHẬN HÀNG
+            </div><br>
+            <div class="space-y-1.5">
+                <div class="text-[#6c757d] font-semibold text-base">
+                    1. Tôi có thể kiểm tra sản phẩm khi nhận hàng không?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    2. Làm thế nào để tôi theo dõi tiến trình xử lý đơn hàng tại IVY moda?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    3. Làm thế nào để nhận hàng nhanh?
+                </div>
+            </div><br>
+
+            <div class="text-lg font-semibold">
+                IV. ĐỔI TRẢ VÀ BẢO HÀNH
+            </div><br>
+            <div class="space-y-1.5">
+                <div class="text-[#6c757d] font-semibold text-base">
+                    1. Chính sách đổi trả sản phẩm tại IVY moda như thế nào?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    2. Chính sách bảo hành tại IVY moda như thế nào?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    3. Những sản phẩm nào tôi không thể đổi/trả?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    4. Tôi sẽ nhận được tiền hoàn trả trong bao lâu?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    5. Tôi phải đóng gói sản phẩm đổi trả, bảo hành như thế nào?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    6. Tôi phải gửi sản phẩm đến đâu để bảo hành?
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    7. Những sản phẩm như thế nào thì được bảo hành?
+                </div>
+            </div><br>
+
+
+            <div class="text-lg font-semibold">
+                V. BẢO QUẢN SẢN PHẨM
+            </div><br>
+            <div class="space-y-1.5">
+                <div class="text-[#6c757d]  font-semibold text-base">
+                    BẢO QUẢN THEO CHẤT LIỆU
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    BẢO QUẢN FORM/DÁNG SP
+                </div>
+                <div class="text-[#6c757d] font-semibold text-base">
+                    BẢO QUẢN MÀU SP
+                </div>
+            </div>
+
+
     </article>
+
     <hr>
 </body>
 <?php require_once "./includes/footer.php" ?>
